6207a933fbc0441061631d80011950744154964a
Anna NodzyÅnska <nodzynska@gmail.com>
2021-07-21 15:08:23 +0200

    Add DataValidator class methods declarations

--- src/main/java/com/ironhack/FabFour/homework2/common/DataValidator.java
+++ src/main/java/com/ironhack/FabFour/homework2/common/DataValidator.java
@@ -1,4 +1,11 @@
 package com.ironhack.FabFour.homework2.common;
 
 public class DataValidator {
+    public boolean validateEmail() {return false;}
+    public boolean validatePhoneNumber() {return false;}
+    public boolean isEmpty() {return false;}
+    public boolean exists() {return false;}
+    public boolean inputLength() {return false;}
+    public boolean isDuplicate() {return false;}
+
 }
